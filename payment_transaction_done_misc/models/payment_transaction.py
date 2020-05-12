c5bfb65fe4013ca83317e232ca50c3b43e69c545
Informatica Arelux <informatica@arelux.com>
2020-05-12 07:06:23 +0200

    fix

--- payment_transaction_done_misc/models/payment_transaction.py
+++ payment_transaction_done_misc/models/payment_transaction.py
@@ -22,10 +22,10 @@ class PaymentTransaction(models.Model):
         if state_done_now==True:
             #done_sale_order_customer_mail_template_id
             if self.acquirer_id.done_sale_order_customer_mail_template_id.id>0:
-                if self.sale_order_id.id>0:
+                for sale_order_id in self.sale_order_ids:
                     #send_mail
-                    if self.sale_order_id.user_id.id>0:
-                        mail_compose_message_obj = self.env['mail.compose.message'].sudo(self.sale_order_id.user_id.id).create({})
+                    if sale_order_id.user_id.id>0:
+                        mail_compose_message_obj = self.env['mail.compose.message'].sudo(sale_order_id.user_id.id).create({})
                     else:                
                         mail_compose_message_obj = self.env['mail.compose.message'].sudo().create({})
                     #onchange_template_id
@@ -34,8 +34,8 @@ class PaymentTransaction(models.Model):
                     #update                                    
                     mail_compose_message_obj.composition_mode = 'comment'
                     mail_compose_message_obj.model = 'sale.order'
-                    mail_compose_message_obj.res_id = self.sale_order_id.id
-                    mail_compose_message_obj.record_name = self.sale_order_id.name                
+                    mail_compose_message_obj.res_id = sale_order_id.id
+                    mail_compose_message_obj.record_name = sale_order_id.name                
                     mail_compose_message_obj.template_id = self.acquirer_id.done_sale_order_customer_mail_template_id.id
                     mail_compose_message_obj.body = mail_body
                     mail_compose_message_obj.subject = return_mail_compose_message_obj['value']['subject']                  
@@ -43,21 +43,21 @@ class PaymentTransaction(models.Model):
                     mail_compose_message_obj.send_mail_action()
             #done_sale_order_user_id_note                    
             if self.acquirer_id.done_sale_order_user_id_note==True:
-                if self.sale_order_id.id>0:
+                for sale_order_id in self.sale_order_ids:
                     mail_message_vals = {
                         'subtype_id': 2,
                         'message_type': 'notification',
                         'body': 'Pago confirmado por un importe de '+str(self.amount)+str(self.currency_id.symbol)+ ' desde '+str(self.acquirer_id.name),
                         'model': 'sale.order',
-                        'res_id': self.sale_order_id.id,
-                        'record_name': self.sale_order_id.name                                                                        
+                        'res_id': sale_order_id.id,
+                        'record_name': sale_order_id.name                                                                        
                     }
                     #add_auto_starred
-                    if self.sale_order_id.user_id.id>0:
-                        mail_message_vals['starred_partner_ids'] = [[6, False, [self.sale_order_id.user_id.partner_id.id]]]
+                    if sale_order_id.user_id.id>0:
+                        mail_message_vals['starred_partner_ids'] = [[6, False, [sale_order_id.user_id.partner_id.id]]]
                     #create
-                    if self.sale_order_id.user_id.id>0:
-                        mail_message_obj = self.env['mail.message'].sudo(self.sale_order_id.user_id.id).create(mail_message_vals)
+                    if sale_order_id.user_id.id>0:
+                        mail_message_obj = self.env['mail.message'].sudo(sale_order_id.user_id.id).create(mail_message_vals)
                     else:
                         mail_message_obj = self.env['mail.message'].sudo().create(mail_message_vals)
             #done_account_journal_id_account_payment
