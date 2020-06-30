2f19cd1d032903f155fda4b1c01070379f8ba9f3
Informatica Arelux <informatica@arelux.com>
2020-06-30 08:14:38 +0200

    https://grupoarelux.atlassian.net/browse/ODOOARELUX-881

--- payment_transaction_done_misc/models/payment_transaction.py
+++ payment_transaction_done_misc/models/payment_transaction.py
@@ -41,23 +41,32 @@ class PaymentTransaction(models.Model):
                     mail_compose_message_obj.subject = return_mail_compose_message_obj['value']['subject']                  
                     #send_mail_action
                     mail_compose_message_obj.send_mail_action()
-            #done_sale_order_user_id_note                    
-            if self.acquirer_id.done_sale_order_user_id_note==True:
-                for sale_order_id in self.sale_order_ids:
+            #done_sale_order_user_id_mail_template_id
+            if self.acquirer_id.done_sale_order_user_id_mail_template_id.id > 0:
+                if self.sale_order_id.id > 0:
+                    mail_compose_message_obj = self.env['mail.compose.message'].sudo().create({})
+                    # onchange_template_id
+                    return_mail_compose_message_obj = mail_compose_message_obj.onchange_template_id(
+                        self.acquirer_id.done_sale_order_user_id_mail_template_id.id, 'comment',
+                        'payment.transaction', self.id)
+                    mail_body = return_mail_compose_message_obj['value']['body']
+                    # create
                     mail_message_vals = {
                         'subtype_id': 2,
                         'message_type': 'notification',
-                        'body': 'Pago confirmado por un importe de '+str(self.amount)+str(self.currency_id.symbol)+ ' desde '+str(self.acquirer_id.name),
+                        'body': mail_body,
                         'model': 'sale.order',
-                        'res_id': sale_order_id.id,
-                        'record_name': sale_order_id.name                                                                        
+                        'res_id': self.sale_order_id.id,
+                        'record_name': self.sale_order_id.name
                     }
-                    #add_auto_starred
-                    if sale_order_id.user_id.id>0:
-                        mail_message_vals['starred_partner_ids'] = [[6, False, [sale_order_id.user_id.partner_id.id]]]
-                    #create
-                    if sale_order_id.user_id.id>0:
-                        mail_message_obj = self.env['mail.message'].sudo(sale_order_id.user_id.id).create(mail_message_vals)
+                    # add_auto_starred
+                    if self.sale_order_id.user_id.id > 0:
+                        mail_message_vals['starred_partner_ids'] = [
+                            [6, False, [self.sale_order_id.user_id.partner_id.id]]]
+                    # create
+                    if self.sale_order_id.user_id.id > 0:
+                        mail_message_obj = self.env['mail.message'].sudo(self.sale_order_id.user_id.id).create(
+                            mail_message_vals)
                     else:
                         mail_message_obj = self.env['mail.message'].sudo().create(mail_message_vals)
             #done_account_journal_id_account_payment
